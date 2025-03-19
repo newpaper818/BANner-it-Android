2e9c553a817711d1a809986e63441a9bdbb87bb1
sejong <snm40109@gmail.com>
2025-03-19 14:37:42 +0900

    ui: add TitleText using on ListGroupCard

---
+++ core/ui/ui/src/main/java/com/fitfit/core/ui/ui/item/TitleText.kt
@@ -0,0 +1,22 @@
+package com.fitfit.core.ui.ui.item
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+
+@Composable
+fun TitleText(
+    text: String,
+    modifier: Modifier = Modifier
+){
+    Text(
+        text = text,
+        style = MaterialTheme.typography.labelMedium.copy(
+            fontWeight = FontWeight.Bold, color = MaterialTheme.colorScheme.onSurfaceVariant
+        ),
+        modifier = modifier.fillMaxWidth()
+    )
+}
