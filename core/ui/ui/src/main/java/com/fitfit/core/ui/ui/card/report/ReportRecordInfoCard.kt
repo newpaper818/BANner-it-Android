08ef6f46ffc3deaeb5389e41678fd59ada7c442f
sejong <snm40109@gmail.com>
2025-03-31 19:37:54 +0900

    ui: add BannerStatusLabel

--- core/ui/ui/src/main/java/com/fitfit/core/ui/ui/card/report/ReportRecordInfoCard.kt
+++ core/ui/ui/src/main/java/com/fitfit/core/ui/ui/card/report/ReportRecordInfoCard.kt
@@ -4,28 +4,25 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.fitfit.core.model.data.DateTimeFormat
 import com.fitfit.core.model.report.BannerInfo
 import com.fitfit.core.model.report.ReportRecord
 import com.fitfit.core.model.report.ReportStatus
+import com.fitfit.core.ui.designsystem.components.BannerStatusLabel
 import com.fitfit.core.ui.designsystem.components.button.EditBannerStatusButton
 import com.fitfit.core.ui.designsystem.components.utils.MyCard
 import com.fitfit.core.ui.designsystem.components.utils.MySpacerColumn
@@ -133,26 +130,10 @@ private fun BannerStatusRow(
         Box(
             modifier = Modifier.padding(vertical = 14.dp)
         ) {
-            Box(
-                contentAlignment = Alignment.Center,
-                modifier = Modifier
-                    .clip(CircleShape)
-                    .background(bannerStatus.color)
-            ) {
-                Text(
-                    text = stringResource(bannerStatus.textId),
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = bannerStatus.textColor,
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.End,
-                    modifier = Modifier.padding(10.dp, 2.dp)
-                )
-            }
+            BannerStatusLabel(bannerStatus)
         }
 
-
         Spacer(modifier = Modifier.weight(1f))
-
 
         AnimatedVisibility(
             visible = showEditBannerStatusButton,
