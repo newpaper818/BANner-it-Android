0dd053293c542a8e3c2629207e3e9bcf405068c5
sejong <snm40109@gmail.com>
2025-04-20 17:57:32 +0900

    new version: 0.1.1-beta (3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.fitfit.bannerit"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.1.0-beta"
+        versionCode = 3
+        versionName = "0.1.1-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
